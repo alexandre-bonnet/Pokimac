8e7eb37deb7f97f52ae84b4d6df49927f036ad7f
Alexandre Bonnet <alexandre.bonnet@edu.esiee.fr>
2025-12-03 11:59:44 +0100

    V1 de Index

---
+++ src/script/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("click",enter);
+
+function enter(){
+    window.location.href='./src/pages/home.html';
+    //document.getElementById("copyright").innerHTML = "YOU CLICKED ME!"
+}
