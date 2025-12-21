292bb67e2450f70b64655ee087d53d3c9f1843e3
LenaFournier <fnrlena@gmail.com>
2025-12-21 17:01:28 +0100

    added nav bar (without audio on every pages)

--- src/script/pokidex.js
+++ src/script/pokidex.js
@@ -44,3 +44,22 @@ document.addEventListener("DOMContentLoaded", () => { // pour lancer des trucs a
   getPokemon();
 });
 
+//audio de la nav bar
+const audio = document.getElementById("bg-music");
+const imgs = document.querySelectorAll(".nav-buttons img");
+let x = null;
+
+function changeState(newState) {
+  imgs.forEach(img => img.classList.remove("active"));
+  x = newState;
+  imgs[x].classList.add("active");
+
+  if (x == 1) {
+    audio.play();
+  } else if (x == 2) {
+    audio.pause();
+  }
+}
+
+document.getElementById("play-btn").addEventListener("click", () => changeState(1));
+document.getElementById("pause-btn").addEventListener("click", () => changeState(2));
