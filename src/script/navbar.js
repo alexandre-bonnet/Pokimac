7c200ed62fc0f1d2474519382e17211ac436772c
CorentinAT <corentin.al2004@gmail.com>
2025-12-21 23:09:07 +0100

    Correction sémantique et simplification de la lecture audio

--- src/script/navbar.js
+++ src/script/navbar.js
@@ -1,22 +1,9 @@
 const audio = document.getElementById("bg-music");
-const imgs = document.querySelectorAll(".nav-buttons img");
-let x = null;
 
-function changeState(newState) {
-  imgs.forEach((img) => img.classList.remove("active"));
-  x = newState;
-  imgs[x].classList.add("active");
-
-  if (x == 1) {
-    audio.play();
-  } else if (x == 2) {
-    audio.pause();
-  }
+function playMusic() {
+  audio.play();
 }
 
-document
-  .getElementById("play-btn")
-  .addEventListener("click", () => changeState(1));
-document
-  .getElementById("pause-btn")
-  .addEventListener("click", () => changeState(2));
+function pauseMusic() {
+  audio.pause();
+}
