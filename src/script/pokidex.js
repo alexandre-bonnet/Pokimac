692da738da1b72539a90d5be92cae1067d8c0022
LenaFournier <fnrlena@gmail.com>
2025-12-21 15:17:53 +0100

    added css in pokidex

--- src/script/pokidex.js
+++ src/script/pokidex.js
@@ -17,13 +17,16 @@ async function getPokemon() {
     const row = document.createElement('tr')
     const imageTd = document.createElement('td')
     const img = document.createElement('img')
+    img.classList.add("img-pokemon")
     img.src = pokemon.image
     imageTd.appendChild(img)
     const nameTd = document.createElement('td')
     nameTd.innerText = pokemon.name
     const typeTd = document.createElement('td')
+    typeTd.classList.add("type-container")
     for (let type of pokemon.types) {
       const imgType = document.createElement('img')
+      imgType.classList.add("img-type")
       imgType.src = type.image
       typeTd.appendChild(imgType)
     }
