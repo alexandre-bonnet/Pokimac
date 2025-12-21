d81db00b7813ef288f1b4be9cb57c69138028f40
CorentinAT <corentin.al2004@gmail.com>
2025-12-22 00:02:01 +0100

    urls pour nginx

--- src/script/index.js
+++ src/script/index.js
@@ -2,5 +2,5 @@ document.addEventListener("click", enter);
 document.addEventListener("keypress", enter);
 
 function enter() {
-  window.location.href = "./src/pages/home.html";
+  window.location.href = "/home";
 }
