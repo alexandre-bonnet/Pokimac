762af09aff9cd71f0f84263599d70b949cfdeeed
LenaFournier <fnrlena@gmail.com>
2025-12-21 18:24:42 +0100

    navBar on every pages

--- src/script/game.js
+++ src/script/game.js
@@ -158,3 +158,24 @@ function publishScore(e) {
 document.addEventListener("DOMContentLoaded", () => {
   getRandomPokemons();
 });
+
+//audio de la nav bar
+const audio = document.getElementById("bg-music");
+const imgs = document.querySelectorAll(".nav-buttons img");
+let x = null;
+
+function changeState(newState) {
+  imgs.forEach(img => img.classList.remove("active"));
+  x = newState;
+  imgs[x].classList.add("active");
+
+  if (x == 1) {
+    audio.play();
+  } else if (x == 2) {
+    audio.pause();
+  }
+}
+
+document.getElementById("play-btn").addEventListener("click", () => changeState(1));
+document.getElementById("pause-btn").addEventListener("click", () => changeState(2));
+
