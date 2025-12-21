2cd5c43bffb9ba7b0ff30bcddcac653cef6598ca
CorentinAT <corentin.al2004@gmail.com>
2025-12-21 17:44:08 +0100

    scores js

---
+++ src/script/scores.js
@@ -0,0 +1,36 @@
+async function getScores() {
+  const generation = document.getElementById("gen-select");
+
+  const response = await fetch(
+    `https://pokimac-api.super-sympa.fr/scores?generation=${generation.value}`
+  );
+  const data = await response.json();
+
+  const listScores = document.getElementById("scores-list");
+  listScores.innerHTML = "";
+
+  if (data.scores.length === 0) {
+    const emptyText = document.createElement("p");
+    emptyText.innerText = "Aucun score dans cette génération pour le moment.";
+    listScores.appendChild(emptyText);
+    return;
+  }
+
+  const sortedScores = data.scores.sort((a, b) => a.score - b.score < 0);
+
+  for (let scoreIdx in sortedScores) {
+    const row = document.createElement("tr");
+    const rankTd = document.createElement("td");
+    rankTd.innerText = +scoreIdx + 1;
+    const nameTd = document.createElement("td");
+    nameTd.innerText = sortedScores[scoreIdx].username;
+    const scoreTd = document.createElement("td");
+    scoreTd.innerText = sortedScores[scoreIdx].score;
+    row.append(rankTd, nameTd, scoreTd);
+    listScores.appendChild(row);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getScores();
+});
